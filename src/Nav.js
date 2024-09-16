309deb144fbe0c90c50a6de5813af855b798ebb4
LixinDu <lx.du@outlook.com>
2024-09-16 22:04:55 +0200

    add Nav.css

--- src/Nav.js
+++ src/Nav.js
@@ -1,27 +1,30 @@
-import Logo from "./Logo";
-import Home from "./Home";
-import About from "./About";
+// import Logo from "./Logo";
+import logo from './assets/Logo.svg';
+import './Nav.css'
+// import About from "./About";
 import { Link, Routes, Route } from 'react-router-dom';
 
 function Nav(){
     return(
-        <div>
-        <nav>
-            <Logo/>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/reservations">Reservations</Link></li>
-                <li><Link to="/orderonline">Order Online</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
-        </nav>
-
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-        </Routes>
+        <div className="grid-container">
+            <nav className="navbar">
+                <div className="logo">
+                    <img src={logo} alt="logo"></img>
+                </div>
+                <ul className="nav-links">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/menu">Menu</Link></li>
+                    <li><Link to="/reservations">Reservations</Link></li>
+                    <li><Link to="/orderonline">Order online</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                {/* <Route path="/" element={<></>}/>
+                <Route path="/about" element={<About/>}/> */}
+                <Route></Route>
+            </Routes>
         </div>
     );
 };
